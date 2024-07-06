e553a2252fe029f00c8de30aa87652e51434df2d
AyeItsHarry <134330512+AyeItsHarry@users.noreply.github.com>
2024-07-06 09:15:00 -0700

    Update WLP
    WII ROOM BABY HELL YEAH

--- wiilink.sh
+++ wiilink.sh
@@ -5,7 +5,7 @@ proot-distro install debian
 proot-distro login debian -- apt update
 proot-distro login debian -- apt upgrade -y
 proot-distro login debian -- apt install wget libicu72 -y
-proot-distro login debian -- wget -O WLP https://github.com/WiiLink24/WiiLink24-Patcher/releases/download/v2.0.2-1/WiiLinkPatcher_Linux-ARM64_v202-1
+proot-distro login debian -- wget -O WLP https://github.com/WiiLink24/WiiLink24-Patcher/releases/download/v2.0.3/WiiLinkPatcher_Linux-ARM64_v203
 proot-distro login debian -- chmod +x WLP
 proot-distro login debian -- ./WLP
 proot-distro login debian -- cp -r WAD/ /sdcard/WAD
