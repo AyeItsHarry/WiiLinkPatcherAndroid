d77256d121b9841275cfb63c6dafd55a6e38d9b2
AyeItsHarry <134330512+AyeItsHarry@users.noreply.github.com>
2024-07-06 08:24:48 -0700

    Add files via upload

---
+++ wiilink.sh
@@ -0,0 +1,12 @@
+pkg update
+pkg upgrade -y
+pkg install proot-distro -y
+proot-distro install debian
+proot-distro login debian -- apt update
+proot-distro login debian -- apt upgrade -y
+proot-distro login debian -- apt install wget libicu72 -y
+proot-distro login debian -- wget -O WLP https://github.com/WiiLink24/WiiLink24-Patcher/releases/download/v2.0.2-1/WiiLinkPatcher_Linux-ARM64_v202-1
+proot-distro login debian -- chmod +x WLP
+proot-distro login debian -- ./WLP
+proot-distro login debian -- cp -r WAD/ /sdcard/WAD
+proot-distro login debian -- cp -r apps/ /sdcard/apps
