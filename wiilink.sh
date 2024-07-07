379bc97ffbbf10c813ec295412d7411b08728f68
AyeItsHarry <134330512+AyeItsHarry@users.noreply.github.com>
2024-07-06 17:06:40 -0700

    Make copying messages more verbose, and store patched files in a subfolder

--- wiilink.sh
+++ wiilink.sh
@@ -8,5 +8,9 @@ proot-distro login debian -- apt install wget libicu72 -y
 proot-distro login debian -- wget -O WLP https://github.com/WiiLink24/WiiLink24-Patcher/releases/download/v2.0.3/WiiLinkPatcher_Linux-ARM64_v203
 proot-distro login debian -- chmod +x WLP
 proot-distro login debian -- ./WLP
-proot-distro login debian -- cp -r WAD/ /sdcard/WAD
-proot-distro login debian -- cp -r apps/ /sdcard/apps
+proot-distro login debian -- mkdir /sdcard/WiiLink
+echo Copying patched WADs...
+proot-distro login debian -- cp -r WAD/ /sdcard/WiiLink/WAD
+echo Copying apps...
+proot-distro login debian -- cp -r apps/ /sdcard/WiiLink/apps
+echo Done! You can find the WADs and apps folders in the "WiiLink" folder on your phone's internal storage. Thanks for using my patcher, I hope you have a nice day!
