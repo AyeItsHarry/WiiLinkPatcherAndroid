8b581f01e6b4db969880b6b31bed978ac088e238
AyeItsHarry <134330512+AyeItsHarry@users.noreply.github.com>
2024-07-19 17:00:03 +0100

    Create ubuntu.sh
    New script that runs within the proot environment to speed up the script

---
+++ ubuntu.sh
@@ -0,0 +1,14 @@
+apt update
+apt upgrade -y
+apt install wget libicu74 -y
+DOWNLOAD_URL=$(curl -L -s https://api.github.com/repos/WiiLink24/WiiLink24-Patcher/releases/latest | grep -o -E "https://(.*)WiiLinkPatcher_Linux-ARM64(.*)")
+DOWNLOAD_URL=${DOWNLOAD_URL::-1}
+wget -O WLP $DOWNLOAD_URL
+chmod +x WLP
+./WLP
+mkdir /sdcard/WiiLink
+echo Copying patched WADs...
+cp -r WAD/ /sdcard/WiiLink/WAD
+echo Copying apps...
+cp -r apps/ /sdcard/WiiLink/apps
+echo "Done. You can find the WADs and apps folders in the WiiLink folder on your device's internal storage. For use on a real Wii console, these folders should be copied to the root of your SD card. Thanks for using my patcher, I hope you have a nice day."
