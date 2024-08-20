2f3d83fc1a0a0db4a0d8f84f35fd248430023a79
ayeitsharry <ayeitsharry@proton.me>
2024-08-20 18:21:32 +0100

    please work 🙏

--- termux.sh
+++ termux.sh
@@ -1,11 +1,9 @@
 rm -rf /sdcard/WiiLink
-mkdir /sdcard/WiiLink
-cd /sdcard/WiiLink
 pkg install proot-distro -y
 pd i ubuntu
 pd sh ubuntu -- apt update
 pd sh ubuntu -- apt upgrade -y
 pd sh ubuntu -- apt install wget -y
-pd sh ubuntu -- wget -O /sdcard/WiiLink/ubuntu.sh https://raw.githubusercontent.com/AyeItsHarry/WiiLinkPatcherAndroid/main/ubuntu.sh
-pd sh ubuntu -- chmod +x /sdcard/WiiLink/ubuntu.sh
-pd sh ubuntu -- /sdcard/WiiLink/ubuntu.sh
+pd sh ubuntu -- wget -O ubuntu.sh https://raw.githubusercontent.com/AyeItsHarry/WiiLinkPatcherAndroid/main/ubuntu.sh
+pd sh ubuntu -- chmod +x ubuntu.sh
+pd sh ubuntu -- ./ubuntu.sh
--- ubuntu.sh
+++ ubuntu.sh
@@ -1,3 +1,4 @@
+mkdir /sdcard/WiiLink
 cd /sdcard/WiiLink
 apt install libicu-dev -y
 DOWNLOAD_URL=$(curl -L -s https://api.github.com/repos/WiiLink24/WiiLink24-Patcher/releases/latest | grep -o -E "https://(.*)WiiLinkPatcher_Linux-ARM64(.*)")
@@ -6,5 +7,4 @@ wget -O WLP $DOWNLOAD_URL
 chmod +x WLP
 ./WLP
 rm /sdcard/WiiLink/WLP
-rm /sdcard/WiiLink/ubuntu.sh
 echo "Done. You can find the WADs and apps folders in the WiiLink folder on your device's internal storage. For use on a real Wii console, these folders should be copied to the root of your SD card. Thanks for using my patcher, I hope you have a nice day."
